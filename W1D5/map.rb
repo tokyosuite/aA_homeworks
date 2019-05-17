e76d9b429e576e70f1eccac167d17ec0353622c8
Helen Yu <helenyueyu@gmail.com>
2019-05-17 08:22:17 -0700

    Finish map ADT.

--- W1D5/map.rb
+++ W1D5/map.rb
@@ -2,5 +2,28 @@
 # Keys should always be unique 
 #
 class Map
+  def initialize
+   @my_map = []
+  end
 
+  def set(key, value)
+    if @my_map.map{|x| x[0]}.include?(key)
+      @my_map.delete_if{|k, v| k == key}
+      @my_map.push([key, value])
+    else
+      @my_map.push([key, value])
+    end 
+  end
+
+  def get(key)
+    @my_map.select{|x| x[0] == key}
+  end
+
+  def delete(key)
+    @my_map.delete_if{|k, v| k == key}
+  end
+
+  def show
+    @my_map
+  end
 end
