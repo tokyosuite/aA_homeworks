84658419fac87e784d284b63b41dfe455a0ccaa8
Helen Yu <helenyueyu@gmail.com>
2019-05-19 21:00:39 -0700

    Reorganize some files. Figure out how to get color back into Rspec (thank you Frankie). Add initialize methods @sequence_length, @game_over, @seq.

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,7 +4,9 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-    
+    @sequence_length = 1
+    @game_over = false
+    @seq = [] 
   end
 
   def play
@@ -12,11 +14,15 @@ class Simon
   end
 
   def take_turn
-
+    show_sequence()
+    if game_over == false 
+      round_success_message
+      sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +30,7 @@ class Simon
   end
 
   def add_random_color
-
+    seq.push(COLORS[rand(0..COLORS.size-1)])
   end
 
   def round_success_message
